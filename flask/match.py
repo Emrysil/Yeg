1eccc2f354e40560e4504a2b1437e3bcf6bc2ad0
yitong <ssunyitong@gmail.com>
2023-09-30 18:13:28 +0800

    add matchScore

--- flask/match.py
+++ flask/match.py
@@ -10,14 +10,21 @@ demo_candidates = [{'id': "1",
                 'gender': 'F',
                 'birthYear': 2018,
                 'education': 'Degree',
-                'skillSet': 'Python, Programming, frontend, backend, UI'
+                'skillSet': 'Program Direction, Leadership, Youth Mentoring, Mentoring, Hosting Services, Live Streaming, Social Media Consulting, Cross-functional Team Leadership, Content Creation, Organization Skills, Agile Project Management, Online Content Creation, Start-ups, Community Building, Team Management, Operations Management, Dance, Event Management, Film Production, Social Media, Production Planning, Project Management, Program Management, Public Speaking, Community Management, Video Production'
                 }, 
                 {'id': "2", 
                 'name': "li sizhuang", 
                 'gender': 'F',
                 'birthYear': 2077,
                 'education': 'Primary School',
-                'skillSet': 'driving, electric engineering, Java'
+                'skillSet': 'Acting, Creative Writing, Film Editing, Film Production, Final Cut Pro, Mac OS, Microsoft Office, Radio Broadcasting, Social Media, Sociology, Teaching, Theatre, Microsoft Excel, Event Management, Public Speaking, Event Planning, Public Relations, Social Media Marketing, Management'
+                }, 
+                {'id': "3", 
+                'name': "li sizhuang 2 号", 
+                'gender': 'F',
+                'birthYear': 2088,
+                'education': 'Primary School',
+                'skillSet': 'Communication and Information, Radio and Television Broadcasting, Public relations, Pitching Ideas, Team Management, Creative Ideation, Project Management, Content Strategy, Social Media Marketing, Program Facilitation, Digital Marketing, Social Media, Teamwork, Customer Service, Interacting with people'
                 }, 
                 ]
 
@@ -27,7 +34,7 @@ entry = (896,
          'Infocomm Technology', 
          # '2023-10-31 01:30:00', 
          datetime(2023, 10, 31, 1, 30 ,0),
-         'This is a software engineer position with expertise in Python, Django, and web development')
+         'Possess a Degree in any discipline or equivalent  At least 8 to 10 years of relevant experience with industry exposure to B2B organisations  Strong communication and interpersonal skills  Track record in supporting senior stakeholders on their business needs Possess drive and able to thrive in a dynamic environment  Results-driven and takes ownership and accountability  Experience with end-to-end HR programmes; including but not limited to leading projects,   campus recruitment fairs, recruitment branding  Strong business acumen and analytical skills Proactive and a team player')
 
 p = {"id": entry[0],
     "name": entry[1],
@@ -42,7 +49,7 @@ def calculate_fuzzy_match_score(str1, str2):
     return fuzz.ratio(str1, str2)
 
 def match(candidates: list, position: dict):
-    nlp = spacy.load("en_core_web_lg")
+    nlp = spacy.load("en_core_web_sm")
 
     '''
     job_description = position["description"] 
@@ -72,6 +79,13 @@ def match(candidates: list, position: dict):
     for i, candidate in enumerate(sorted_candidates):
         print(f"Rank {i + 1}: {candidate['name']} - Similarity Score: {candidate['score']:.4f}")
 
+    scores = [d["score"] for d in sorted_candidates]
+    n = len(scores)
+    max_score = max(scores)
+    for d in sorted_candidates:
+        d["matchingScore"] = "{:.1f}".format(d["score"] / max_score * 100)
+    for d in sorted_candidates:
+        print(d)
     for candiate in sorted_candidates:
         candiate.pop("score", None)
 
